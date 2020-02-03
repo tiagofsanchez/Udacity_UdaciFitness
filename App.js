6d6240720b5c7c7e9fc146ffb23f166bf1ca83e3
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-02-04 01:21:40 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import AddEntry from "./components/AddEntry";
+
+export default function App() {
+  return (
+    <View>
+      <AddEntry />
+    </View>
+  );
+}
