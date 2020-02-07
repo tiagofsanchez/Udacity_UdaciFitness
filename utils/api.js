59ae996c63bbe4da26c29cd5fd26f46dbae7105a
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2020-02-07 15:57:05 +0800

    FEATURE: started to work on the history view

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,11 @@
 import { AsyncStorage } from "react-native";
-import { CALENDAR_STORAGE_KEY } from "./_calendar";
+import { CALENDAR_STORAGE_KEY, formatCalendarResults } from "./_calendar";
+
+
+export function fetchCalendarResult( ) { 
+  return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+  .then(formatCalendarResults)
+}
 
 export function submitEntry({ entry, key }) {
   return AsyncStorage.mergeItem(
